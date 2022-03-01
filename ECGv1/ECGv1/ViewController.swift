12365b9933ee6ce77200edd80f2b8b462a6f2250
Andrew Toader <69984232+aet37@users.noreply.github.com>
2022-02-28 19:59:33 -0500

    Update Bod Rate

--- ECGv1/ECGv1/ViewController.swift
+++ ECGv1/ECGv1/ViewController.swift
@@ -14,6 +14,7 @@ import UIKit
 import CoreBluetooth
 import Charts
 
+
 class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDelegate {
     
     /*-------------------------------Bluetooth-------------------------------*/
