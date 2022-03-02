3cdd2c168c58dcb1e318e797ec965c94d94627ec
ljs108 <84594342+ljs108@users.noreply.github.com>
2022-03-02 15:14:08 -0500

    committing the bridge header

--- ECGv1/TestAdd/ECGv1-Bridging-Header.h
+++ ECGv1/TestAdd/ECGv1-Bridging-Header.h
@@ -3,8 +3,8 @@
 //
 
 
-#include 'addAB_data.h'
-#include 'addAB_initialize.h'
-#include 'addAB_terminate.h'
-#include 'addAB_types.h'
-#include 'addAB.h"
+#include <addAB_data.h>
+#include <addAB_initialize.h>
+#include <addAB_terminate.h>
+#include <addAB_types.h>
+#include <addAB.h>
