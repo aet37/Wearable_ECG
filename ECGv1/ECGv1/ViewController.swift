ebd7f513cfba2bdf2836635d2539beccbbae273c
bobbyrouse <65084125+bobbyrouse@users.noreply.github.com>
2022-04-13 06:36:40 -0400

    final app

--- ECGv1/ECGv1/ViewController.swift
+++ ECGv1/ECGv1/ViewController.swift
@@ -86,8 +86,94 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
         return button3
     }()
     
-//    @IBOutlet weak var Addtest: UILabel!
+    public let dataTitl: UIButton = {
+        let dataTitl = UIButton()
+        //testbutton.backgroundColor = .systemGreen
+        dataTitl.setTitle("User Data", for: .normal)
+        dataTitl.setTitleColor(.white, for: .normal)
+        dataTitl.titleLabel!.font = (UIFont .systemFont(ofSize: 25))
+        //testbutton.layer.cornerRadius = 20
+        dataTitl.layer.masksToBounds = true
+        return dataTitl
+    }()
     
+    public let dispAvg: UIButton = {
+        let dispAvg = UIButton()
+        dispAvg.backgroundColor = .white
+        dispAvg.setTitle("86", for: .normal)
+        dispAvg.setTitleColor(.black, for: .normal)
+        dispAvg.layer.cornerRadius = 20
+        dispAvg.layer.masksToBounds = true
+        return dispAvg
+    }()
+    
+    public let avgTitl: UIButton = {
+        let avgTitl = UIButton()
+        avgTitl.setTitle("Average Heart Rate", for: .normal)
+        avgTitl.setTitleColor(.white, for: .normal)
+        avgTitl.titleLabel!.font = (UIFont .systemFont(ofSize: 17))
+        avgTitl.layer.masksToBounds = true
+        return avgTitl
+    }()
+    
+    public let recTitl: UIButton = {
+        let recTitl = UIButton()
+        recTitl.setTitle("Most Recent Heart Rates", for: .normal)
+        recTitl.setTitleColor(.white, for: .normal)
+        recTitl.titleLabel!.font = (UIFont .systemFont(ofSize: 17))
+        recTitl.layer.masksToBounds = true
+        return recTitl
+    }()
+    
+    public let rec1: UIButton = {
+        let rec1 = UIButton()
+        rec1.backgroundColor = .white
+        rec1.setTitle("86", for: .normal)
+        rec1.setTitleColor(.black, for: .normal)
+        rec1.layer.cornerRadius = 20
+        rec1.layer.masksToBounds = true
+        return rec1
+    }()
+    
+    public let rec2: UIButton = {
+        let rec2 = UIButton()
+        rec2.backgroundColor = .white
+        rec2.setTitle("86", for: .normal)
+        rec2.setTitleColor(.black, for: .normal)
+        rec2.layer.cornerRadius = 20
+        rec2.layer.masksToBounds = true
+        return rec2
+    }()
+    
+    public let rec3: UIButton = {
+        let rec3 = UIButton()
+        rec3.backgroundColor = .white
+        rec3.setTitle("86", for: .normal)
+        rec3.setTitleColor(.black, for: .normal)
+        rec3.layer.cornerRadius = 20
+        rec3.layer.masksToBounds = true
+        return rec3
+    }()
+    
+    public let rec4: UIButton = {
+        let rec4 = UIButton()
+        rec4.backgroundColor = .white
+        rec4.setTitle("86", for: .normal)
+        rec4.setTitleColor(.black, for: .normal)
+        rec4.layer.cornerRadius = 20
+        rec4.layer.masksToBounds = true
+        return rec4
+    }()
+    
+    /*-------------------------------Data Page-------------------------------*/
+    let scrollView = UIScrollView()
+    
+    let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = 2
+        pageControl.backgroundColor = .systemBlue
+        return pageControl
+    }()
     /*-------------------------------Main-------------------------------*/
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -98,24 +184,42 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
         //view.backgroundColor = .systemGray
         //imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         //imageView.center = view.center
-        
-        
-        
         view.addSubview(button2)
         view.addSubview(button3)
+        
         self.lineChartView.leftAxis.axisMinimum = 0
         self.lineChartView.rightAxis.axisMinimum = 0
+
+        scrollView.delegate = self
+        pageControl.addTarget(self, action: #selector(pageControlDidChange(_:)),
+                              for: .valueChanged)
+        view.addSubview(scrollView)
+        view.addSubview(pageControl)
+
         
         initChart()
    
         // Notification to call chart updater every time something is recived in queue
         NotificationCenter.default.addObserver(self, selector: #selector(self.updateChartValues), name: Notification.Name("push"), object: nil)
+      
         
     }
-    
-    /*-------------------------------Button Layout-------------------------------*/
+    @objc func pageControlDidChange(_ sender: UIPageControl){
+        let current = sender.currentPage
+        scrollView.setContentOffset(CGPoint(x: CGFloat(current) * view.frame.size.width,
+                                            y: 0), animated: true)
+    }
+    var temp = 0
+        /*-------------------------------Button Layout-------------------------------*/
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+        pageControl.frame = CGRect(x: 10, y: view.frame.size.height-50, width: view.frame.size.width-20, height: 30)
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height-65)
+        
+        if scrollView.subviews.count == 2 {
+            configureScrollView()
+        }
         button2.frame = CGRect(
             x: 57,
             y: 185,
@@ -129,6 +233,90 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
             height: 55
         )
         
+        dataTitl.frame = CGRect(
+            x: view.frame.size.width / 2 - 60,
+            y: 70,
+            width: 120,
+            height: 55
+        )
+        
+        avgTitl.frame = CGRect(
+            x: view.frame.size.width / 4 - 55,
+            y: 185,
+            width: 150,
+            height: 55
+        )
+        
+        dispAvg.frame = CGRect(
+            x: view.frame.size.width - 145,
+            y: 185,
+            width: 80,
+            height: 55
+        )
+        
+        recTitl.frame = CGRect(
+            x: view.frame.size.width / 2 - 100,
+            y: 250,
+            width: 200,
+            height: 55
+        )
+        
+        rec1.frame = CGRect(
+            x: view.frame.size.width / 2 - 30,
+            y: 300,
+            width: 60,
+            height: 55
+        )
+        
+        rec2.frame = CGRect(
+            x: view.frame.size.width / 2 - 30,
+            y: 360,
+            width: 60,
+            height: 55
+        )
+        
+        rec3.frame = CGRect(
+            x: view.frame.size.width / 2 - 30,
+            y: 420,
+            width: 60,
+            height: 55
+        )
+        
+        rec4.frame = CGRect(
+            x: view.frame.size.width / 2 - 30,
+            y: 480,
+            width: 60,
+            height: 55
+        )
+        
+    }
+    
+    func configureScrollView() {
+        scrollView.contentSize = CGSize(width: view.frame.size.width*2, height: scrollView.frame.size.height)
+        scrollView.isPagingEnabled = true
+        let colors: [UIColor] = [.systemRed, .systemBlue]
+        for x in 0..<2{
+            let page = UIView(frame: CGRect(x: CGFloat(x) * view.frame.size.width,
+                                            y: 0,
+                                            width: view.frame.size.width,
+                                            height: scrollView.frame.size.height))
+            if(x == 1){
+                page.backgroundColor = colors[x]
+            }
+            scrollView.addSubview(page)
+            if(x == 1){
+                page.addSubview(dataTitl)
+                page.addSubview(dispAvg)
+                page.addSubview(avgTitl)
+                page.addSubview(recTitl)
+                page.addSubview(rec1)
+                page.addSubview(rec2)
+                page.addSubview(rec3)
+                page.addSubview(rec4)
+            }
+        }
+        
+        
     }
     
     
@@ -137,11 +325,11 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
     /*-------------------------------Chart Values-------------------------------*/
     
     let numVal = 1000
-    
     var valuesArr = Array<ChartDataEntry>(repeating: ChartDataEntry(x: Double(0), y: Double(0)), count: 1000)
     var currVal = 0.0
-    
-    //var globalFilterTime = 0.0
+    var avgHeartRate = 0.0
+    var totalHeartRate = [0.0]
+    var recentHeartRate = [0.0, 0.0, 0.0, 0.0] // keep at 4
     
     // Listen for pushed() notification
     
@@ -183,11 +371,24 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
             valuesArr[999].y = (currVal * 0.2) + (0.8 * valuesArr[999].y)
             currVal = valuesArr[999].y
             //filtering
-           // var currVal = valuesArr[0].y
+            // var currVal = valuesArr[0].y
             //for i in 0...999{
             //    currVal = (currVal * 0.2) + (0.8 * valuesArr[i].y)
             //    valuesArr[i].y = currVal
-           // }
+            // }
+            
+            //update average heartrate
+            //totalHeartRate.append("lucio HR value")
+            //avgHeartRate = totalHeartRate.reduce(0, +) / Double(totalHeartRate.count)
+            //dispAvg.setTitle(String(format: "%.2f", avgHeartRate), for: .normal)
+            
+            //update most recent heart rates
+//            recentHeartRate.removeFirst()
+//            recentHeartRate.append("lucio HR value")
+//            rec1.setTitle(String(recentHeartRate[3]), for: .normal)
+//            rec2.setTitle(String(recentHeartRate[2]), for: .normal)
+//            rec3.setTitle(String(recentHeartRate[1]), for: .normal)
+//            rec4.setTitle(String(recentHeartRate[0]), for: .normal)
             
         }
         let set1 = LineChartDataSet(entries: valuesArr, label: "EKG")
@@ -198,72 +399,6 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
         
         self.lineChartView.data = data
     }
-    /*
-    @objc func updateChartValues(arr: Array<ChartDataEntry>){
-        if(EKGQueue.isEmpty() == false){
-            var newVal = Double(EKGQueue.pop())
-                        
-            // newVal = continuousFilter(newVal: newVal, oldVal: valuesArr[0])
-           // newVal = continuousFilter(arr: newVal)
-            
-            valuesArr.removeFirst()
-            valuesArr.append(ChartDataEntry(x: Double(999), y: Double(newVal)))
-            
-            valuesArr = continuousFilter(arr: valuesArr)
-            
-            //valuesArr = arr
-            for i in 0..<numVal {
-                valuesArr[i].x = Double(i)
-                valuesArr[i].y = Double(valuesArr[i].y)
-            }
-            
-            
-            
-        }
-        for i in 0...999{
-            if(i<100){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=100 && i<200){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=200 && i<300){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=300 && i<400){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=400 && i<500){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=500 && i<600){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=600 && i<700){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=700 && i<800){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=800 && i<900){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=900 && i<1000){
-                valuesArr[i].y = Double(0)
-            }
-            
-        }
-        
-        valuesArr = continuousFilter(arr: valuesArr)
-        let set1 = LineChartDataSet(entries: valuesArr, label: "EKG")
-        set1.drawCirclesEnabled = false
-        set1.drawValuesEnabled = false
-        set1.lineWidth = 3.0
-        let data = LineChartData(dataSet: set1)
-        
-        self.lineChartView.data = data
-    }
- */
     
     @objc func initChart(){
         for i in 0..<numVal {
@@ -284,88 +419,8 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
         let data = LineChartData(dataSet: set1)
         
         self.lineChartView.data = data
-        //let set1 = LineChartDataSet(entries: valuesArr, label: "EKG")
-        //set1.drawCirclesEnabled = false
-        //let data = LineChartData(dataSet: set1)
-       
-        //self.lineChartView.data = data
-        /*
-        var testECGdata = getCSVData(dataFile: "/Users/bobbyrouse/Downloads/Wearable_ECG/ECGv1/ECGv1/data1.csv");
-        for i in 0..<numVal {
-            valuesArr[i] = ChartDataEntry(x: Double(i), y: Double(testECGdata[0][i]))
-        }
-        for i in 0...999{
-            if(i<100){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=100 && i<200){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=200 && i<300){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=300 && i<400){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=400 && i<500){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=500 && i<600){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=600 && i<700){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=700 && i<800){
-                valuesArr[i].y = Double(0)
-            }
-            if(i>=800 && i<900){
-                valuesArr[i].y = Double(50)
-            }
-            if(i>=900 && i<1000){
-                valuesArr[i].y = Double(0)
-            }
-            
-        }
-        
-        currVal = valuesArr[0].y
-        for i in 0...999{
-            currVal = (currVal * 0.2) + (0.8 * valuesArr[i].y)
-            valuesArr[i].y = currVal
-        }
-        
-        //valuesArr = continuousFilter(arr: valuesArr)
-        let set1 = LineChartDataSet(entries: valuesArr, label: "EKG")
-        set1.drawCirclesEnabled = false
-        let data = LineChartData(dataSet: set1)
-        
-        self.lineChartView.data = data
-        */
     }
-    
-    func continuousFilter(arr: Array<ChartDataEntry>) -> Array<ChartDataEntry>{
-        //convert to y-only array for filtering
-        var yArr = Array<Float>(repeating: 0.0, count: 1000)
-        
-        for i in 0...999 {
-            yArr[i] = Float(arr[i].y)
-        }
-        
-        //calls NVDSP library
-        let bandpass : NVBandpassFilter = NVBandpassFilter(samplingRate: 115.0)
-        bandpass.centerFrequency = 19.75
-        bandpass.q = 0.10101010101
-        bandpass.filterData(&yArr, numFrames: 1000, numChannels: 1)
-        
-        //convert back values
-        
-        for i in 0...999 {
-            arr[i].y = Double(yArr[i])
-        }
-        
-        
-        return arr
-    }
+
     
     /*-------------------------------Bluetooth-------------------------------*/
     
@@ -471,4 +526,11 @@ class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDe
         centralManager.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey : false])
     }
     
+}
+
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(floorf(Float(scrollView.contentOffset.x) / Float(scrollView.frame.size.width)))
+    }
 }
